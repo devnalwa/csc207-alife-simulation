985ee4790092770deedfa645cde602083678c49c
Kohei <Kohei@132.161.246.85>
2017-12-03 11:17:44 -0600

    added methods

--- src/Organism.java
+++ src/Organism.java
@@ -1,11 +1,36 @@
 import java.util.Map;
 
 public abstract class Organism {
+    int energy;
 	
-	public Organism(Map<String, Integer> counts) {
-		
+	public Organism() {
+	    this.energy = 0;
 	}
 	
+	void update() {
+	    this.energy++;
+	}
 	
+	int getEnergy() {
+	    return this.energy;
+	}
+	
+	void incrementEnergy() {
+	    this.energy++;
+	}
+	
+	void decrementEnergy() {
+	    if(this.energy > 0) {
+	        this.energy--;
+	    }
+	}
+	
+	abstract String getType();
+	
+	abstract Organism reproduce();
+	
+	abstract double getCooperationProbability();
+	
+	abstract boolean cooperates()
 
 }
--- src/Population.java
+++ src/Population.java
@@ -1,4 +1,20 @@
 
 public class Population {
+    
+    public Population(Map<String, Integer> counts) {
+        
+    }
+    
+    void update() {
+        
+    }
+    
+    double calculateCooperationMean() {
+        
+    }
+    
+    Map<Organism, Integer> getPopulationCounts() {
+        
+    }
 
 }
